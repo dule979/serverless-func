40b3edd50bbc7492c6138d43e4bc1ca6be33c210
Dusan Ilic <dule979@gmail.com>
2023-12-28 18:24:41 +0100

    First commit

---
+++ assets/data.js
@@ -0,0 +1,36 @@
+const items = [
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    name: 'utopia sofa',
+    image: {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbQAoVZbGzoug56TdGd6ya3Vemzzi2OVBNDlxvEQi-DL_Ha7biy9bmDz2EjZxBzyyHXro&usqp=CAU',
+    },
+    price: 39.95,
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    name: 'entertainment center',
+    image: {
+      url: 'https://www.woodwaves.com/cdn/shop/products/Mid_Century_Modern_Entertainment_Center_Floating_TV_Stand_-_Arc_Mocha_992x992.jpg?v=1571436353',
+    },
+    price: 29.98,
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    name: 'albany sectional',
+    image: {
+      url: 'https://images.furnituredealer.net/img/products%2Falbany%2Fcolor%2F2214--515892002_2214-2pc-gens-17692-b1.jpg',
+    },
+    price: 10.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    name: 'leather sofa',
+    image: {
+      url: 'https://assets.wfcdn.com/im/38510137/resize-h800-w800%5Ecompr-r85/2083/208369192/Ardrie+82.5%27%27+Mid-Century+Modern+Furniture+Style+Genuine+Leather+Chesterfield+Sofa.jpg',
+    },
+    price: 9.99,
+  },
+];
+
+module.exports = items;
