925819b9960e9ce0e04bad0af62d0e0e6cd3a501
Dusan Ilic <dule979@gmail.com>
2023-12-28 18:55:49 +0100

    Added "Access-Control-Allow-Origin" to basic-api

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const data = require('../assets/data');
 
 exports.handler = async (event, context) => {
   return {
+    Headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
     statusCode: 200,
     body: JSON.stringify(data),
   };
